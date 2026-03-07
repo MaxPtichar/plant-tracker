372b7c03437108ca320e87112e95e6cd2940d25f
MaxPtichar <dart546@gmail.com>
2026-03-07 20:12:45 +0300

    chore(add_new_measurement): Add curly brackets to control drop timing

--- plant_tracker/src/add_new_measurement.rs
+++ plant_tracker/src/add_new_measurement.rs
@@ -2,22 +2,27 @@ use chrono::NaiveDate;
 use teloxide::types;
 
 use crate::models::{Measurement, MeasurementType, Plant, PlantType};
+use crate::build_app::get_avr_r_for_each_plant;
 
 use crate::storage::{load, save};
 
 
 pub fn add_new_measurement(plants:  &mut Vec<Plant>, id: u32, weight: f32, date: NaiveDate, type_: MeasurementType) {
-    
-
-    let plant = find_plant(plants, id).unwrap();
-
     let measure = Measurement {
         weight,
         date,
         type_,
-    };
+};
+
+{
+    let plant = find_plant(plants, id).unwrap();
+
+    
+    
 
     plant.measurements.push(measure);
+}
+    get_avr_r_for_each_plant(plants);
     save(&plants);
 }
 
