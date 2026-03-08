3111c48c83edb9f16c18367fa72a37dc9f9b1892
MaxPtichar <dart546@gmail.com>
2026-03-08 21:28:47 +0300

    style: apply cargo fmt formatting, deleted unused libs

--- plant_tracker/src/main.rs
+++ plant_tracker/src/main.rs
@@ -1,23 +1,16 @@
+mod add_new_measurement;
 mod analytics;
-mod bot_api;
+mod bot;
 mod build_app;
 mod constants;
 mod input;
 mod models;
 mod storage;
-mod  add_new_measurement;
 
-use crate::bot_api::plant_bot;
-use crate::build_app::get_avr_r_for_each_plant;
-use crate::storage::load;
+
 
 #[tokio::main]
 async fn main() {
-    // let mut plants = load();
 
-    // get_avr_r_for_each_plant(&mut plants);
-
-    // let menu = build_app::menu(&mut plants);
-
-    plant_bot().await;
+    bot::plant_bot().await;
 }
