a823f42099704f79f2b2fc2dfe4d2fa9150dc9b9
MaxPtichar <dart546@gmail.com>
2026-03-08 21:27:22 +0300

    style: apply cargo fmt formatting

--- plant_tracker/src/add_new_measurement.rs
+++ plant_tracker/src/add_new_measurement.rs
@@ -1,34 +1,33 @@
 use chrono::NaiveDate;
 use teloxide::types;
 
-use crate::models::{Measurement, MeasurementType, Plant, PlantType};
 use crate::build_app::get_avr_r_for_each_plant;
+use crate::models::{Measurement, MeasurementType, Plant, PlantType};
 
 use crate::storage::{load, save};
 
-
-pub fn add_new_measurement(plants:  &mut Vec<Plant>, id: u32, weight: f32, date: NaiveDate, type_: MeasurementType) {
+pub fn add_new_measurement(
+    plants: &mut Vec<Plant>,
+    id: u32,
+    weight: f32,
+    date: NaiveDate,
+    type_: MeasurementType,
+) {
     let measure = Measurement {
         weight,
         date,
         type_,
-};
+    };
 
-{
-    let plant = find_plant(plants, id).unwrap();
+    {
+        let plant = find_plant(plants, id).unwrap();
 
-    
-    
-
-    plant.measurements.push(measure);
-}
+        plant.measurements.push(measure);
+    }
     get_avr_r_for_each_plant(plants);
     save(&plants);
 }
 
-
-
 fn find_plant<'a>(plants: &'a mut Vec<Plant>, choosen_id: u32) -> Option<&'a mut Plant> {
     plants.iter_mut().find(|p| p.id == choosen_id)
 }
-
