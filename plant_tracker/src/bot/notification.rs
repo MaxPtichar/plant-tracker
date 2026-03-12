6ce5fbbc19d866ea0c410aaa059c4edd9673ef52
MaxPtichar <dart546@gmail.com>
2026-03-12 15:14:41 +0300

    fmt notifications

--- plant_tracker/src/bot/notification.rs
+++ plant_tracker/src/bot/notification.rs
@@ -1,34 +1,31 @@
 use crate::analytics::days_until_watering;
-use crate::storage::load;
 use crate::bot::user::load_chat_id;
 use crate::models::watering_status;
+use crate::storage::load;
 
 use teloxide::Bot;
 use teloxide::prelude::Requester;
 
-pub async fn chat_notification (bot: &Bot) {
-    let Some(chat_id) = load_chat_id() else {return };
+pub async fn chat_notification(bot: &Bot) {
+    let Some(chat_id) = load_chat_id() else {
+        return;
+    };
     let plant = load();
 
-    let urgent: Vec<String> = plant.iter().filter_map(|plant| {
-        let days = days_until_watering(plant)?;
-        if days <= 2.0 {
-            Some(format!("🌱 {}: {}", plant.name, watering_status(days)))
-            
-        } else { None }
-
-}).collect();
-
+    let urgent: Vec<String> = plant
+        .iter()
+        .filter_map(|plant| {
+            let days = days_until_watering(plant)?;
+            if days <= 2.0 {
+                Some(format!("🌱 {}: {}", plant.name, watering_status(days)))
+            } else {
+                None
+            }
+        })
+        .collect();
 
     if !urgent.is_empty() {
         let text = format!("💧 Пора поливать:\n{}", urgent.join("\n"));
         let _ = bot.send_message(chat_id, text).await;
     }
-
-    }
-
-    
-
-
-
-
+}
