4937249538db69dd81959f6e7e04fa23c09fecd1
MaxPtichar <dart546@gmail.com>
2026-03-12 15:11:20 +0300

    feat(operations): add last feed display formatting

--- plant_tracker/src/operations.rs
+++ plant_tracker/src/operations.rs
@@ -1,10 +1,30 @@
-use crate::analytics::{days_until_watering, get_avg_r};
+use crate::analytics::{days_until_watering, get_avg_r, last_plant_feed};
 
+use crate::days_from_last_feed;
 use crate::models::{Measurement, MeasurementType, Plant, watering_status};
-use crate::storage::{load, save};
+use crate::storage::save;
 
 use chrono::NaiveDate;
 
+pub fn last_feed(plants: &Vec<Plant>) -> String {
+    let mut lines: Vec<String> = Vec::new();
+    for plant in plants {
+        let feed = match last_plant_feed(plant) {
+            Some(value) => format!(
+                "🌿 {} \n   ┗ последняя подкормка: {} ({} дн. назад) 🫧 ",
+                plant.name,
+                value,
+                days_from_last_feed(plant)
+            ),
+            None => format!("🪴 {} \n   ┗ подкормок не было 🫙", plant.name),
+        };
+        lines.push(feed);
+    }
+
+    lines.join("\n")
+}
+
+//get average evapuation rate for each plant
 pub fn get_avr_r_for_each_plant(plants: &mut [Plant]) {
     for plant in plants.iter_mut() {
         let avg = get_avg_r(plant);
@@ -12,6 +32,7 @@ pub fn get_avr_r_for_each_plant(plants: &mut [Plant]) {
     }
 }
 
+// return days untill last watering
 pub fn get_predicate(plants: &Vec<Plant>) -> String {
     let mut lines: Vec<String> = Vec::new();
 
@@ -28,6 +49,7 @@ pub fn get_predicate(plants: &Vec<Plant>) -> String {
     lines.join("\n")
 }
 
+// add new measurement
 pub fn add_new_measurement(
     plants: &mut Vec<Plant>,
     id: u32,
@@ -50,6 +72,7 @@ pub fn add_new_measurement(
     save(&plants);
 }
 
+//find plant in vec<Plants> from load() func
 fn find_plant<'a>(plants: &'a mut Vec<Plant>, choosen_id: u32) -> Option<&'a mut Plant> {
     plants.iter_mut().find(|p| p.id == choosen_id)
 }
