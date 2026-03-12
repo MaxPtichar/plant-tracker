78efd84e4cbbea4784da8f01ae5970fb9dd32e41
MaxPtichar <dart546@gmail.com>
2026-03-12 15:14:58 +0300

    fmt user.rs

--- plant_tracker/src/bot/user.rs
+++ plant_tracker/src/bot/user.rs
@@ -1,21 +1,15 @@
-use teloxide::types::{ ChatId};
 use std::fs;
+use teloxide::types::ChatId;
 
 pub fn save_chat_id(chat_id: ChatId) {
-    
     fs::create_dir_all("data").unwrap();
-    
+
     fs::write("data/chat_id.txt", chat_id.0.to_string()).unwrap();
-
 }
-
 
 pub fn load_chat_id() -> Option<ChatId> {
     match fs::read_to_string("data/chat_id.txt") {
         Ok(content) => content.trim().parse::<i64>().ok().map(ChatId),
         Err(_) => None,
-        
     }
-    
-    
 }
