2fc47109313b3695f04e7fc2ba00dc3ad039ad08
MaxPtichar <dart546@gmail.com>
2026-03-12 15:14:16 +0300

    chore(mod): add d == "LastFeed" to filter in CallBackQuery

--- plant_tracker/src/bot/mod.rs
+++ plant_tracker/src/bot/mod.rs
@@ -1,9 +1,9 @@
 pub mod callbacks;
-pub mod user;
 pub mod commands;
 pub mod dialogue;
-pub mod notification;
 pub mod keyboards;
+pub mod notification;
+pub mod user;
 use std::time::Duration;
 
 pub use commands::Command;
@@ -14,15 +14,15 @@ use teloxide::dispatching::Dispatcher;
 use teloxide::dispatching::dialogue::{self as tg_dialogue, InMemStorage};
 use teloxide::utils::command::BotCommands;
 
-use teloxide::prelude::*;
 use chrono::{Local, Timelike};
+use teloxide::prelude::*;
 
 use crate::bot::callbacks::cancel_callback;
 use crate::bot::commands::{handle_command, handle_menu_buttons};
 use crate::bot::dialogue::{receive_plant, receive_type, receive_weight, recieve_date};
+use crate::bot::notification::chat_notification;
 use crate::operations;
 use crate::storage::{load, save};
-use crate::bot::notification::chat_notification;
 
 pub type MyDialogue = Dialogue<MeasurementDialogue, InMemStorage<MeasurementDialogue>>;
 pub type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
@@ -33,8 +33,8 @@ pub async fn plant_bot() {
     let bot = Bot::from_env();
 
     let bot_clone = bot.clone();
-     tokio::spawn(notification_loop(bot_clone));
-    
+    tokio::spawn(notification_loop(bot_clone));
+
     bot.set_my_commands(commands::Command::bot_commands())
         .await
         .unwrap();
@@ -62,7 +62,7 @@ pub async fn plant_bot() {
             .branch(
                 dptree::filter(|q: CallbackQuery| {
                     q.data.as_deref().map_or(false, |d| {
-                        d == "status" || d == "Addmeasurement" || d == "Cancel"
+                        d == "status" || d == "Addmeasurement" || d == "LastFeed" || d == "Cancel"
                     })
                 })
                 .endpoint(handle_menu_buttons),
@@ -92,19 +92,13 @@ pub async fn plant_bot() {
         .await;
 }
 
-
-
-
-async fn notification_loop(bot_clone: Bot) { 
-   
-        loop {
-            let now = Local::now();
-            if now.hour() == 10 && now.minute() == 0 {
+async fn notification_loop(bot_clone: Bot) {
+    loop {
+        let now = Local::now();
+        if now.hour() == 10 && now.minute() == 0 {
             chat_notification(&bot_clone.clone()).await;
             tokio::time::sleep(Duration::from_secs(60)).await;
-            }
-            tokio::time::sleep(Duration::from_secs(30)).await;
         }
+        tokio::time::sleep(Duration::from_secs(30)).await;
     }
-    
-
+}
