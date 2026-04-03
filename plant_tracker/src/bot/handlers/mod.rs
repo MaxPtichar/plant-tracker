82ee205b39ebbdb5479ee69edd0a670676d8fd1b
MaxPtichar <dart546@gmail.com>
2026-04-03 20:58:04 +0300

    add new imports

--- plant_tracker/src/bot/handlers/mod.rs
+++ plant_tracker/src/bot/handlers/mod.rs
@@ -1,5 +1,7 @@
 pub mod measurement;
 pub mod plant_creation;
 pub mod plants;
+pub mod delete_plants;
 pub mod pot_creation;
+pub mod measurements_record;
 pub use measurement::{receive_date, receive_type, receive_weight};
