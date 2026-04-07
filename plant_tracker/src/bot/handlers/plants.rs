7bb6e2711977d6575a39cfc1a24a00c857e745ef
MaxPtichar <dart546@gmail.com>
2026-04-07 18:50:17 +0300

    refactor(plants): update get_all_plants_status witn new logic

--- plant_tracker/src/bot/handlers/plants.rs
+++ plant_tracker/src/bot/handlers/plants.rs
@@ -1,6 +1,8 @@
+use std::clone;
+
 use sqlx::PgPool;
 
-use crate::{analytics::days_until_watering, db_operations, models::watering_status};
+use crate::{analytics::days_until_watering, analytics_new::{days_until_watering_full, get_outdoor_temp}, db_operations, models::{PlantFullContext, watering_status}};
 
 /// Returns a formatted watering status string for all user's plants.
 ///
@@ -15,42 +17,133 @@ use crate::{analytics::days_until_watering, db_operations, models::watering_stat
 /// - `"не настроено"` if no active pot config or no watering recorded
 /// - `"Пока еще нет ни одного растения🌱"` if user has no plants
 pub async fn get_all_plants_status(pool: &PgPool, chat_id: i64) -> sqlx::Result<String> {
-    let plants = db_operations::get_user_plants(&pool, chat_id).await?;
+    let plants = db_operations::get_all_plants_data(&pool, chat_id).await?;
     if plants.is_empty() {
         return Ok(format!("Пока еще нет ни одного растения🌱"));
     }
+
+    
     let mut result: Vec<String> = Vec::new();
+    let temp_outdoor= get_outdoor_temp();
 
     for plant in &plants {
-        let measurements = db_operations::recieve_two_last_measurement(&pool, plant.id).await?;
-        let pot = db_operations::get_active_config(&pool, plant.id).await?;
-        let target_moisture = plant.target_moisture;
-        let after_watering_weight =
-            db_operations::get_last_watering_weight(&pool, plant.id).await?;
-        dbg!(&pot);
-        dbg!(&after_watering_weight);
+        let PlantFullContext {
+            current_weight,
+            last_watering_weight,
+            pot_weight,
+            dry_soil_weight,
+            soil_type,
+            plant_type,
+            light_level, 
+            air_circulation, 
+            pot_diameter_cm, 
+            transpiration_coef, 
+            avg_r, 
+            cycles_count,
+            plants_name,
+            plant_id,
 
-        let line = match (pot, after_watering_weight) {
-            (Some(pot), Some(after_watering_weight)) => {
-                match days_until_watering(
-                    &measurements,
-                    &pot,
-                    target_moisture,
-                    after_watering_weight,
-                ) {
-                    Some(days) => format!("🌱 {} — {}", plant.plants_name, watering_status(days)),
-                    None => format!("🌱 {} — нет данных", plant.plants_name),
-                }
-            }
+            ..
 
-            _ => format!("🌱 {} — не настроено", plant.plants_name),
+        } =&plant;
+
+        let dry_total = (pot_weight + dry_soil_weight) as f32;
+        let dry_soil_weight_g = *dry_soil_weight as f32;
+
+        let regular_measurements = db_operations::recieve_two_last_measurement(pool, *plant_id).await?;
+
+
+        dbg!((last_watering_weight, avg_r));
+
+        let ( after_watering_weight, avg_r) =
+        match ( last_watering_weight, avg_r)  {
+            
+            ( &Some(l_watering), &Some(r)) => ( l_watering.clone(), r.clone()),
+            _=>  { result.push(format!("{plants_name} - недостаточно данных для расчета").to_string()); continue;}
+
+            
+            
+        }; 
+        
+
+
+        let days = days_until_watering_full(
+            &regular_measurements, 
+            after_watering_weight, 
+            dry_total, soil_type, 
+            dry_soil_weight_g, 
+            plant_type, 
+            light_level, 
+            air_circulation, 
+            pot_diameter_cm.clone(), 
+            transpiration_coef.clone(), 
+            avg_r, 
+            cycles_count.clone(), 
+            temp_outdoor); 
+
+
+        let line = match days {
+    
+                    Some(d) => format!("🌱 {} — {}",plants_name, watering_status(d)),
+                    _ => format!("🌱 {} — нет данных", plants_name),
+                
+            
+
         };
 
+
         result.push(line);
+
     }
 
-    Ok(result.join("\n"))
+     Ok(result.join("\n"))
+
 }
+
+
+
+
+
+
+
+
+// pub async fn get_all_plants_status_old(pool: &PgPool, chat_id: i64) -> sqlx::Result<String> {
+//     let plants = db_operations::get_user_plants(&pool, chat_id).await?;
+//     if plants.is_empty() {
+//         return Ok(format!("Пока еще нет ни одного растения🌱"));
+//     }
+//     let mut result: Vec<String> = Vec::new();
+
+//     for plant in &plants {
+//         let measurements = db_operations::recieve_two_last_measurement(&pool, plant.id).await?;
+//         let pot = db_operations::get_active_config(&pool, plant.id).await?;
+//         let target_moisture = 2.0;
+//         let after_watering_weight =
+//             db_operations::get_last_watering_weight(&pool, plant.id).await?;
+//         dbg!(&pot);
+//         dbg!(&after_watering_weight);
+
+//         let line = match (pot, after_watering_weight) {
+//             (Some(pot), Some(after_watering_weight)) => {
+//                 match days_until_watering(
+//                     &measurements,
+//                     &pot,
+//                     target_moisture,
+//                     after_watering_weight,
+//                 ) {
+//                     Some(days) => format!("🌱 {} — {}", plant.plants_name, watering_status(days)),
+//                     None => format!("🌱 {} — нет данных", plant.plants_name),
+//                 }
+//             }
+
+//             _ => format!("🌱 {} — не настроено", plant.plants_name),
+//         };
+
+//         result.push(line);
+//     }
+
+//     Ok(result.join("\n"))
+// }
 
 /// Returns a formatted list of all user's plants with their configuration details.
 ///
@@ -79,7 +172,7 @@ pub async fn get_list_of_all_plants(pool: &PgPool, chat_id: i64) -> sqlx::Result
          📊 Последнее измерение: {}\n\
         ",
                 plant.plants_name,
-                (plant.target_moisture * 100.0).round(),
+                (0.3 * 100.0),
                 plant.pot_weight,
                 plant.dry_soil_weight,
                 plant
